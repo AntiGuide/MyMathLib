b44b5143dfd25df7565623802aaca5dfa3b49c55
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-11-14 15:14:39 +0100

    Implemented quaternion class + some tests

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaternion {
+    class Program {
+        static void Main(string[] args) {
+            var a = new Quaternion(1, 2, 3, 4);
+            var b = new Quaternion(8, 7, 6, 5);
+            var c = new Quaternion(9, 11, 13, 10);
+
+            Console.Write("a) \t" + ((a + b + c) == (c + b + a)).ToString() + " \t");
+            Console.WriteLine((a+b+c).ToString() + "\t\t = \t" + (c+b+a).ToString());
+
+            Console.Write("b) \t" + ((a * b) == (b * a)).ToString() + " \t");
+            Console.WriteLine((a * b).ToString() + "\t\t = \t" + (b * a).ToString());
+
+            Console.Write("c) \t" + (((a + b) * c) == (a * c + b * c)).ToString() + " \t");
+            Console.WriteLine(((a + b) * c).ToString() + "\t = \t" + (a*c + b*c).ToString());
+
+            Console.Write("d) \t" + (((a * b) * c) == (a * (b * c))).ToString() + " \t");
+            Console.WriteLine(((a * b) * c).ToString() + "\t = \t" + (a * (b * c)).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Quaternion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaternion {
+    class Quaternion {
+        public double x, y, z, w;
+
+        public Quaternion(double x, double y, double z, double w) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public static Quaternion operator+(Quaternion a, Quaternion b) {
+            var x = a.x + b.x;
+            var y = a.y + b.y;
+            var z = a.z + b.z;
+            var w = a.w + b.w;
+
+            return new Quaternion(x, y, z, w);
+        }
+
+        public static Quaternion operator*(Quaternion a, Quaternion b) {
+            var x = a.x * b.x - a.y * b.y - a.z * b.z - a.w * b.w;
+            var y = a.x * b.y + a.y * b.x + a.z * b.w - a.w * b.z;
+            var z = a.x * b.z - a.y * b.w + a.z * b.x + a.w * b.y;
+            var w = a.x * b.w + a.y * b.z - a.z * b.y + a.w * b.x;
+
+            return new Quaternion(x, y, z, w);
+        }
+
+        public static bool operator==(Quaternion a, Quaternion b) {
+            return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w;
+        }
+
+        public static bool operator !=(Quaternion a, Quaternion b) {
+            return a.x != b.x || a.y != b.y || a.z != b.z || a.w != b.w;
+        }
+
+        override public string ToString() {
+            return x + "+" + y + "i+" + z + "j+" + w + "k";
+        }
+    }
+}
