ee86f3929bdb344e1490d063638f971eceb9f58f
Lukas Wiedemann <lukaswiedemann@gmail.com>
2018-11-14 16:58:20 +0100

    Added checks + Implemented further functionality

--- Quaternion/Program.cs
+++ Quaternion/Program.cs
@@ -11,17 +11,30 @@ namespace Quaternion {
             var b = new Quaternion(8, 7, 6, 5);
             var c = new Quaternion(9, 11, 13, 10);
 
+            Console.WriteLine();
+            Console.WriteLine("Quaternion Tests");
+            Console.WriteLine("----------------");
+            Console.WriteLine();
+
             Console.Write("a) \t" + ((a + b + c) == (c + b + a)).ToString() + " \t");
-            Console.WriteLine((a+b+c).ToString() + "\t\t = \t" + (c+b+a).ToString());
+            Console.WriteLine((a+b+c).ToString() + "\t\t\t= \t" + (c+b+a).ToString());
 
             Console.Write("b) \t" + ((a * b) == (b * a)).ToString() + " \t");
-            Console.WriteLine((a * b).ToString() + "\t\t = \t" + (b * a).ToString());
+            Console.WriteLine((a * b).ToString() + "\t\t\t= \t" + (b * a).ToString());
 
             Console.Write("c) \t" + (((a + b) * c) == (a * c + b * c)).ToString() + " \t");
-            Console.WriteLine(((a + b) * c).ToString() + "\t = \t" + (a*c + b*c).ToString());
+            Console.WriteLine(((a + b) * c).ToString() + "\t\t= \t" + (a*c + b*c).ToString());
 
             Console.Write("d) \t" + (((a * b) * c) == (a * (b * c))).ToString() + " \t");
-            Console.WriteLine(((a * b) * c).ToString() + "\t = \t" + (a * (b * c)).ToString());
+            Console.WriteLine(((a * b) * c).ToString() + "\t\t= \t" + (a * (b * c)).ToString());
+
+            Console.WriteLine();
+
+            Console.Write("Division:");
+            Console.WriteLine("\t" + (a * b) / a);
+
+            Console.Write("Inverse:");
+            Console.WriteLine("\t" + a.Inverse() + "\t= \t" + Quaternion.Conjugate(a) / Math.Pow(a.Abs(), 2));
 
             Console.ReadLine();
         }
--- Quaternion/Quaternion.cs
+++ Quaternion/Quaternion.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quaternion {
-    class Quaternion {
+    public class Quaternion {
         public double x, y, z, w;
 
         public Quaternion(double x, double y, double z, double w) {
@@ -15,7 +11,7 @@ namespace Quaternion {
             this.w = w;
         }
 
-        public static Quaternion operator+(Quaternion a, Quaternion b) {
+        public static Quaternion operator +(Quaternion a, Quaternion b) {
             var x = a.x + b.x;
             var y = a.y + b.y;
             var z = a.z + b.z;
@@ -24,7 +20,7 @@ namespace Quaternion {
             return new Quaternion(x, y, z, w);
         }
 
-        public static Quaternion operator*(Quaternion a, Quaternion b) {
+        public static Quaternion operator *(Quaternion a, Quaternion b) {
             var x = a.x * b.x - a.y * b.y - a.z * b.z - a.w * b.w;
             var y = a.x * b.y + a.y * b.x + a.z * b.w - a.w * b.z;
             var z = a.x * b.z - a.y * b.w + a.z * b.x + a.w * b.y;
@@ -33,15 +29,47 @@ namespace Quaternion {
             return new Quaternion(x, y, z, w);
         }
 
+        public static Quaternion operator /(Quaternion a, Quaternion b) {
+            var bGes2 = Math.Pow(b.x, 2) + Math.Pow(b.y, 2) + Math.Pow(b.z, 2) + Math.Pow(b.w, 2);
+
+            var x = a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+            var y = a.y * b.x - a.x * b.y - a.w * b.z + a.z * b.w;
+            var z = a.z * b.x + a.w * b.y - a.x * b.z - a.y * b.w;
+            var w = a.w * b.x - a.z * b.y + a.y * b.z - a.x * b.w;
+
+            return new Quaternion(x, y, z, w) / bGes2;
+        }
+
+        public static Quaternion operator /(Quaternion a, double b) {
+            return new Quaternion(a.x / b, a.y / b, a.z / b, a.w / b);
+        }
+
+        public double Abs() {
+            return Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2) + Math.Pow(this.z, 2) + Math.Pow(this.w, 2));
+        }
+
+        public Quaternion Inverse() {
+            var thisGes2 = Math.Pow(this.x, 2) + Math.Pow(this.y, 2) + Math.Pow(this.z, 2) + Math.Pow(this.w, 2);
+            return Conjugate(this) / thisGes2;
+        }
+
+        public static Quaternion Conjugate(Quaternion a) {
+            return new Quaternion(a.x, -a.y, -a.z, -a.w);
+        }
+
+        public Quaternion Normalize() {
+            return this / this.Abs();
+        }
+
         public static Quaternion operator !(Quaternion a) {
             return new Quaternion(-a.x, -a.y, -a.z, -a.w);
         }
 
-        public static Quaternion operator-(Quaternion a, Quaternion b) {
+        public static Quaternion operator -(Quaternion a, Quaternion b) {
             return a + !b;
         }
 
-        public static bool operator==(Quaternion a, Quaternion b) {
+        public static bool operator ==(Quaternion a, Quaternion b) {
             return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w;
         }
 
@@ -50,7 +78,21 @@ namespace Quaternion {
         }
 
         override public string ToString() {
-            return x + "+" + y + "i+" + z + "j+" + w + "k";
+            return Math.Round(x,2) + "+" + Math.Round(y, 2) + "i+" + Math.Round(z, 2) + "j+" + Math.Round(w, 2) + "k";
+        }
+
+        public override bool Equals(object obj) {
+            var item = obj as Quaternion;
+
+            if (item == null) {
+                return false;
+            }
+
+            return this == item;
+        }
+
+        public override int GetHashCode() {
+            return this.GetHashCode();
         }
     }
 }
