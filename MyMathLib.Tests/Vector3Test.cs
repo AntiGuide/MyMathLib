bd3448c7cab32dce118db47a8088cfb285f59c9a
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2019-09-24 15:23:32 +0200

    Added NUnit

---
+++ MyMathLib.Tests/Vector3Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace MyMathLib.Tests
+{
+    [TestFixture]
+    public class Vector3Test {
+        private Vector3[] vectors;
+    
+        [SetUp]
+        protected void SetUp() {
+            vectors = new[] {
+                new Vector3(0d, 0d, 0d),
+            };
+        }
+
+        [Test]
+        public void Creation() {
+            Assert.AreEqual(vectors[0].x, 0d);
+            Assert.AreEqual(vectors[0].y, 0d);
+            Assert.AreEqual(vectors[0].z, 0d);
+        }
+    }
+}
