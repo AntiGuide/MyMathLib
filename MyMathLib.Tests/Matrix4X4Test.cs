8335c52fdfe2caca74db1b0d6c1c46ef0f936053
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2019-09-26 10:51:59 +0200

    Removed left handed expected values

--- MyMathLib.Tests/Matrix4X4Test.cs
+++ MyMathLib.Tests/Matrix4X4Test.cs
@@ -4,11 +4,10 @@ using NUnit.Framework;
 namespace MyMathLib.Tests {
     [TestFixture]
     public class Matrix4X4Test {
-        private Quaternion quaternion;
-
+        
         [Test]
         public void SetRotationQuaternion() {
-            var qat = new Quaternion(0.7071f, 0f, 0f, 0.7071f);
+            var qat = new Quaternion(0.7071067811865476f, 0f, 0f, 0.7071067811865476f);
             var mat = new Matrix4X4();
             mat.SetRotation(qat);
             var expectedValue = new[,] {
@@ -24,24 +23,9 @@ namespace MyMathLib.Tests {
         public void SetRotationAngleAxis() {
             //throw new NotImplementedException();
         }
-
-        #region SetQuaternionDebug
-        [Test]
-        public void SetRotationEuler1() {
-            var mat = new Matrix4X4();
-            mat.SetRotation(33.3f, 66.6f, 99.9f);
-            Assert.Warn(System.Environment.NewLine + mat);
-            var expectedValue = new[,] {
-                {0.4280839562f, -0.4778637588f, 0.7670660615f, 0f}, 
-                {0.8233616352f, -0.1436996460f, -0.5490228534f, 0f}, 
-                {0.3725852370f, 0.8666006923f, 0.3319391012f, 0f}, 
-                {0f, 0f, 0f, 1f}, 
-            };
-            mat.AssertValues(expectedValue);
-        }
         
         [Test]
-        public void SetRotationEuler2() {
+        public void SetRotationEuler() {
             var mat = new Matrix4X4();
             mat.SetRotation(90f, 0f, 0f);
             var expectedValue = new[,] {
@@ -51,84 +35,36 @@ namespace MyMathLib.Tests {
                 {0f, 0f, 0f, 1f},
             };
             mat.AssertValues(expectedValue);
-        }
-        
-        [Test]
-        public void SetRotationEuler3() {
-            var mat = new Matrix4X4();
+            mat = new Matrix4X4();
             mat.SetRotation(0f, 90f, 0f);
-            //Assert.Warn(System.Environment.NewLine + mat);
-            var expectedValue = new[,] {
+            expectedValue = new[,] {
                 {0f, 0f, 1f, 0f},
                 {0f, 1f, 0f, 0f},
                 {-1f, 0f, 0f, 0f},
                 {0f, 0f, 0f, 1f}, 
             };
             mat.AssertValues(expectedValue);
-        }
-        
-        [Test]
-        public void SetRotationEuler4() {
-            var mat = new Matrix4X4();
+            mat = new Matrix4X4();
             mat.SetRotation(0f, 0f, 90f);
-            //Assert.Warn(System.Environment.NewLine + mat);
-            var expectedValue = new[,] {
+            expectedValue = new[,] {
                 {0f, -1f, 0f, 0f},
                 {1f, 0f, 0f, 0f},
                 {0f, 0f, 1f, 0f},
                 {0f, 0f, 0f, 1f}, 
             };
             mat.AssertValues(expectedValue);
-        }
-        
-        [Test]
-        public void SetRotationEuler5() {
-            var mat = new Matrix4X4();
-            //mat.SetRotation(90f, 0f, 90f);
-            quaternion = new Quaternion(0f,90f,90f);
-            mat.SetRotation(quaternion);
-            Assert.Warn(System.Environment.NewLine + quaternion);
-            Assert.Warn(System.Environment.NewLine + mat);
-            var expectedValue = new[,] {
+            mat = new Matrix4X4();
+            mat.SetRotation(0f,90f,90f);
+            expectedValue = new[,] {
                 {0f, 0f, 1f, 0f},
                 {1f, 0f, 0f, 0f},
                 {0f, 1f, 0f, 0f},
                 {0f, 0f, 0f, 1f}, 
             };
             mat.AssertValues(expectedValue);
-        }
-        
-        [Test]
-        public void SetRotationEuler6() {
-            var mat = new Matrix4X4();
-            var qat = new Quaternion(-45f, -30f, -70f);
-            mat.SetRotation(qat);
-            Assert.Warn(System.Environment.NewLine + qat);
-            Assert.Warn(System.Environment.NewLine + mat);
-            var expectedValue = new[,] {
-                {0.2961981f, -0.8137977f,  0.5000000f, 0f},
-                {0.7853854f, -0.0903867f, -0.6123725f, 0f},
-                {0.5435407f,  0.5740763f,  0.6123725f, 0f},
-                {0f, 0f, 0f, 1f},
-            };
-//            var expectedValue = new[,] {
-//                {0.6284296513f, -0.6928753853f, 0.3535533845f, 0f},
-//                {0.6644630432f, 0.2418446541f, -0.7071068287f, 0f},
-//                {0.4044318497f, 0.6792900562f, 0.6123723984f, 0f},
-//                {0f, 0f, 0f, 1f}, 
-//            };
-            mat.AssertValues(expectedValue);
-        }
-        
-        [Test]
-        public void SetRotationEuler7() {
-            var mat = new Matrix4X4();
-            //mat.SetRotation(90f, 0f, 90f);
-            quaternion = new Quaternion(0f,90f,-90f);
-            mat.SetRotation(quaternion);
-            Assert.Warn(System.Environment.NewLine + quaternion);
-            Assert.Warn(System.Environment.NewLine + mat);
-            var expectedValue = new[,] {
+            mat = new Matrix4X4();
+            mat.SetRotation(0f,90f,-90f);
+            expectedValue = new[,] {
                 {0f, 0f, 1f, 0f},
                 {-1f, 0f, 0f, 0f},
                 {0f, -1f, 0f, 0f},
@@ -136,8 +72,7 @@ namespace MyMathLib.Tests {
             };
             mat.AssertValues(expectedValue);
         }
-        #endregion
-        
+
         [Test]
         public void SetTranslation() {
             var mat = new Matrix4X4();
