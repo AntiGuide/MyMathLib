6a07be73f16e6548f55b4c3ea92d049029dc3e05
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2019-09-26 16:16:39 +0200

    Cleanup

--- MyMathLib.Tests/Vector3Test.cs
+++ MyMathLib.Tests/Vector3Test.cs
@@ -3,92 +3,95 @@
 namespace MyMathLib.Tests {
     [TestFixture]
     public class Vector3Test {
-        private Vector3[] vectors;
-
-        [SetUp]
-        protected void SetUp() {
-            vectors = new[] {
-                new Vector3(0, 0, 0),
-                new Vector3(1, 0, 3),
-                new Vector3(-1, 4, 2),
-                new Vector3(1, 2, 3),
-                new Vector3(1, 5, 7),
-                new Vector3(2, 4, 4),
-                new Vector3(3, 1, 2),
-            };
-        }
-
         [Test]
         public void Creation() {
-            vectors[0].AssertValues(0, 0, 0);
+            var vec = new Vector3(0, 0, 0);
+            vec.AssertValues(0, 0, 0);
+            
+            vec = new Vector3();
+            vec.AssertValues(0, 0, 0);
         }
 
         [Test]
         public void GetComponentsIndexer() {
-            Assert.That(vectors[0][0], Is.EqualTo(0).Within(1).Percent);
-            Assert.That(vectors[0][1], Is.EqualTo(0).Within(1).Percent);
-            Assert.That(vectors[0][2], Is.EqualTo(0).Within(1).Percent);
+            var vec = new Vector3(0, 0, 0);
+            vec.AssertValuesIndexer(0,0,0);
         }
 
         [Test]
         public void SetComponentsIndexer() {
-            vectors[0][0] = 5;
-            vectors[0][1] = 4;
-            vectors[0][2] = 3;
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var vec = new Vector3(0, 0, 0);
             
-            Assert.That(vectors[0][0], Is.EqualTo(5).Within(1).Percent);
-            Assert.That(vectors[0][1], Is.EqualTo(4).Within(1).Percent);
-            Assert.That(vectors[0][2], Is.EqualTo(3).Within(1).Percent);
+            vec[0] = 5;
+            vec[1] = 4;
+            vec[2] = 3;
+            
+            vec.AssertValuesIndexer(5,4,3);
         }
 
         [Test]
         public void SetComponents() {
-            vectors[0].x = 1;
-            vectors[0].y = 2;
-            vectors[0].z = 3;
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var vec = new Vector3();
+            
+            vec.x = 1;
+            vec.y = 2;
+            vec.z = 3;
 
-            vectors[0].AssertValues(1, 2, 3);
+            vec.AssertValues(1, 2, 3);
         }
 
         [Test]
         public void Add() {
-            var vec = vectors[1] + vectors[2];
+            var addVec1 = new Vector3(1, 0, 3);
+            var addVec2 = new Vector3(-1, 4, 2);
+            var vec = addVec1 + addVec2;
             vec.AssertValues(0, 4, 5);
         }
 
         [Test]
         public void Subtract() {
-            var vec = vectors[1] - vectors[2];
+            var subVec1 = new Vector3(1, 0, 3);
+            var subVec2 = new Vector3(-1, 4, 2);
+            var vec = subVec1 - subVec2;
             vec.AssertValues(2, -4, 1);
         }
 
         [Test]
         public void ScalarMultiplication() {
-            var vec = 5 * vectors[2];
+            var mulVec = new Vector3(-1, 4, 2);
+            var vec = 5 * mulVec;
             vec.AssertValues(-5, 20, 10);
         }
 
         [Test]
         public void DotProduct() {
-            var res = Vector3.Dot(vectors[1], vectors[2]);
-            Assert.That(res, Is.EqualTo(5).Within(1).Percent);
+            var dotVec1 = new Vector3(1, 0, 3);
+            var dotVec2 = new Vector3(-1, 4, 2);
+            var res = Vector3.Dot(dotVec1, dotVec2);
+            Assert.That(res, Is.EqualTo(5).Within(0.00001).Percent);
         }
 
         [Test]
         public void CrossProduct() {
-            var vec = Vector3.Cross(vectors[3], vectors[4]);
+            var croVec1 = new Vector3(1, 2, 3);
+            var croVec2 = new Vector3(1, 5, 7);
+            var vec = Vector3.Cross(croVec1, croVec2);
             vec.AssertValues(-1, -4, 3);
         }
 
         [Test]
         public void Length() {
-            var res = vectors[5].Length;
-            Assert.That(res, Is.EqualTo(6).Within(1).Percent);
+            var vec = new Vector3(2, 4, 4);
+            var res = vec.Length;
+            Assert.That(res, Is.EqualTo(6).Within(0.00001).Percent);
         }
 
         [Test]
         public void Normalize() {
-            var vec = vectors[6].Normalized;
+            var unoVec = new Vector3(3, 1, 2);
+            var vec = unoVec.Normalized;
             vec.AssertValues(0.801783681f, 0.267261237f, 0.534522474f);
         }
     }
@@ -98,6 +101,12 @@ namespace MyMathLib.Tests {
             Assert.That(vec.x, Is.EqualTo(x).Within(0.00001));
             Assert.That(vec.y, Is.EqualTo(y).Within(0.00001));
             Assert.That(vec.z, Is.EqualTo(z).Within(0.00001));
+        }
+        
+        public static void AssertValuesIndexer(this Vector3 vec, float x, float y, float z) {
+            Assert.That(vec[0], Is.EqualTo(x).Within(0.00001));
+            Assert.That(vec[1], Is.EqualTo(y).Within(0.00001));
+            Assert.That(vec[2], Is.EqualTo(z).Within(0.00001));
         }
     }
 }
--- MyMathLib/Program.cs
+++ MyMathLib/Program.cs
@@ -3,20 +3,18 @@
 namespace MyMathLib {
     public static class Program {
         private static void Main() {
-//            var mat = new Matrix4X4();
-//            mat.SetRotation(-122f, 99f, 66f);//Unity Left Handed We are right handed
-//            point = new Vector3(0,10,0);
-//            point = mat.Transform(point);
-//            Console.WriteLine(point);
-
             var mat = new Matrix4X4();
-            //mat.SetTRS(new Vector3(20,10,5), new Quaternion (60,30,90), new Vector3(3,3,3));
-            mat.SetTRS(new Vector3(20,10,5), new Quaternion (0.6830127f, 0.5f, -0.1830127f, 0.4999999f), new Vector3(3,3,3));
+            mat.SetTRS(new Vector3(20, 0, 0), new Quaternion(0, 0, 90), new Vector3(3, 3, 3));
             var point = new Vector3(0, 10, 0);
             point = mat.Transform(point);
-            //var expectedResultPoint = new Vector3(-10, 0, 0);
+            var expectedResultPoint = new Vector3(-10, 0, 0);
 
-            Console.WriteLine(point);
+            Console.WriteLine($"GotPoint: ({Round(point.x)}, {Round(point.y)}, {Round(point.z)})");
+            Console.WriteLine($"Expected: ({Round(expectedResultPoint.x)}, {Round(expectedResultPoint.y)}, {Round(expectedResultPoint.z)})");
+        }
+
+        private static float Round(float number, int decimals = 5) {
+            return (float) System.Math.Round(number, decimals, MidpointRounding.AwayFromZero);
         }
     }
 }
