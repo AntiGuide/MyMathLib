3f3d1e80a579ab35dc8014d195d92887e204fc61
Lukas Wiedemann <lukaswiedemann@gmail.com>
2019-09-27 22:09:35 +0200

    Commented Matrix4X4 and Program

--- MyMathLib/Matrix4X4.cs
+++ MyMathLib/Matrix4X4.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 
 namespace MyMathLib {
+    /// <summary>Representation of a mathematical 4x4 matrix which can be used to transform points</summary>
     public class Matrix4X4 {
+        /// <summary>The 2D array to represent the combined matrix including translation, rotation and scale</summary>
         private readonly float[,] matrix;
 
+        /// <summary>Matrix containing only the scaling component of the final matrix</summary>
         private Matrix4X4 scaleMatrix;
+
+        /// <summary>Matrix containing only the rotation component of the final matrix</summary>
         private Matrix4X4 rotationMatrix;
+
+        /// <summary>Matrix containing only the translation component of the final matrix</summary>
         private Matrix4X4 translationMatrix;
 
+        /// <summary>
+        /// Create a final matrix from a pre created 2D float array
+        /// </summary>
+        /// <param name="matrix">A 2D float array with 4X4 dimensions to directly set</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the dimensions don't match the matrix dimensions'</exception>
         public Matrix4X4(float[,] matrix) {
             if (matrix.GetLength(0) != 4 || matrix.GetLength(1) != 4) {
                 throw new ArgumentOutOfRangeException(nameof(matrix), "Array height and width has to be 4");
@@ -18,7 +30,11 @@ namespace MyMathLib {
 
             this.matrix = matrix;
         }
-
+        
+        /// <summary>
+        /// Create a final matrix from a pre created one dimensional array or IReadOnlyList
+        /// </summary>
+        /// <param name="tmpMatrix">Array containing 16 elements</param>
         public Matrix4X4(IReadOnlyList<float> tmpMatrix) {
             var matrix2 = new float[4, 4];
             for (int i = 0; i < 4; i++) {
@@ -29,7 +45,11 @@ namespace MyMathLib {
 
             this.matrix = matrix2;
         }
-
+        
+        /// <summary>
+        /// Create a final matrix from a pre created one dimensional list or IEnumerable
+        /// </summary>
+        /// <param name="tmpMatrix">List containing 16 elements</param>
         public Matrix4X4(IEnumerable<float> tmpMatrix) {
             var matrix2 = new float[4, 4];
             var enumerable = tmpMatrix as float[] ?? tmpMatrix.ToArray();
@@ -42,11 +62,15 @@ namespace MyMathLib {
             this.matrix = matrix2;
         }
 
+        /// <summary>Create a matrix from 16 individual floats. m1 - m4 => row 1 and so on</summary>
         public Matrix4X4(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9, float m10, float m11, float m12, float m13, float m14, float m15, float m16) {
             var tmpMatrix = new[,] {{m1, m2, m3, m4}, {m5, m6, m7, m8}, {m9, m10, m11, m12}, {m13, m14, m15, m16}};
             this.matrix = tmpMatrix;
         }
 
+        /// <summary>
+        /// Create an identity matrix
+        /// </summary>
         public Matrix4X4() {
             this.matrix = new float[4, 4];
             matrix[0, 0] = 1f;
@@ -55,6 +79,10 @@ namespace MyMathLib {
             matrix[3, 3] = 1f;
         }
 
+        /// <summary>
+        /// Set the translation component of the final matrix. Used in Matrix4X4.Transform().
+        /// </summary>
+        /// <param name="vec">The translation vector. Points will get transformed by those values.</param>
         public void SetTranslation(Vector3 vec) {
             EnsureMatricesSetUp();
 
@@ -65,6 +93,10 @@ namespace MyMathLib {
             ComputeMatrix();
         }
 
+        /// <summary>
+        /// Set the scale component of the final matrix. Used in Matrix4X4.Transform().
+        /// </summary>
+        /// <param name="vec">The scale vector. Points will get scaled by those values.</param>
         public void SetScale(Vector3 vec) {
             EnsureMatricesSetUp();
 
@@ -75,6 +107,10 @@ namespace MyMathLib {
             ComputeMatrix();
         }
 
+        /// <summary>
+        /// Set the rotation component of the final matrix using a quaternion. Used in Matrix4X4.Transform().
+        /// </summary>
+        /// <param name="q">The quaternion representation of a rotation</param>
         public void SetRotation(Quaternion q) {
             EnsureMatricesSetUp();
 
@@ -95,20 +131,37 @@ namespace MyMathLib {
             ComputeMatrix();
         }
 
+        /// <summary>
+        /// Set the rotation component of the final matrix using an angle axis representation. Used in Matrix4X4.Transform().
+        /// </summary>
+        /// <param name="angle">The angle to rotate around the axis.</param>
+        /// <param name="axis">The axis to rotate around</param>
         public void SetRotation(float angle, Vector3 axis) {
             SetRotation(new Quaternion(angle, axis));
         }
 
+        /// <summary>
+        /// Set the rotation component of the final matrix using euler angles. Used in Matrix4X4.Transform().
+        /// </summary>
         public void SetRotation(float x, float y, float z) {
             SetRotation(new Quaternion(x, y, z));
         }
 
+        /// <summary>
+        /// Sets translation, rotation and scale.
+        /// </summary>
+        /// <param name="translation">Move objects by those values</param>
+        /// <param name="rotation">Rotate objects with this quaternion</param>
+        /// <param name="scale">Scale objects by this values</param>
         public void SetTRS(Vector3 translation, Quaternion rotation, Vector3 scale) {
             SetTranslation(translation);
             SetRotation(rotation);
             SetScale(scale);
         }
 
+        /// <summary>
+        /// Internal method to compute the final matrix out of the translation, rotation and scale components set by SetTRS, SetRotation, SetScale and SetTranslation
+        /// </summary>
         private void ComputeMatrix() {
             // Scale, Rotate, Translate
             //var result = scaleMatrix * rotationMatrix * translationMatrix;
@@ -121,12 +174,20 @@ namespace MyMathLib {
             }
         }
 
+        /// <summary>
+        /// Checks if the components arrays are initialized and initializes them if they are not
+        /// </summary>
         private void EnsureMatricesSetUp() {
             if (scaleMatrix is null) scaleMatrix = new Matrix4X4();
             if (rotationMatrix is null) rotationMatrix = new Matrix4X4();
             if (translationMatrix is null) translationMatrix = new Matrix4X4();
         }
 
+        /// <summary>
+        /// Access the final matrix with an indexer.
+        /// </summary>
+        /// <param name="i">The final matrixes row</param>
+        /// <param name="i2">The final matrixes column</param>
         public float this[int i, int i2] {
             get => matrix[i, i2];
             set => matrix[i, i2] = value;
@@ -218,6 +279,11 @@ namespace MyMathLib {
             return ret;
         }
 
+        /// <summary>
+        /// Transform a point by the matrix
+        /// </summary>
+        /// <param name="point">The point to transform</param>
+        /// <returns>Returns a copy of the transformed point</returns>
         public Vector3 Transform(Vector3 point) {
             return this * point;
         }
--- MyMathLib/Program.cs
+++ MyMathLib/Program.cs
@@ -4,15 +4,23 @@ namespace MyMathLib {
     public static class Program {
         private static void Main() {
             var mat = new Matrix4X4();
+
+            // Set translation, rotation and scale for matrix
             mat.SetTRS(new Vector3(20, 0, 0), new Quaternion(0, 0, 90), new Vector3(3, 3, 3));
+
             var point = new Vector3(0, 10, 0);
+
+            // Transform point with created matrix
             point = mat.Transform(point);
+
             var expectedResultPoint = new Vector3(-10, 0, 0);
 
+            // Print transformed and expected point to compare
             Console.WriteLine($"GotPoint: ({Round(point.x)}, {Round(point.y)}, {Round(point.z)})");
             Console.WriteLine($"Expected: ({Round(expectedResultPoint.x)}, {Round(expectedResultPoint.y)}, {Round(expectedResultPoint.z)})");
         }
 
+        /// <summary>Rounds a float away from zero with 5 decimals as a standard</summary>
         private static float Round(float number, int decimals = 5) {
             return (float) System.Math.Round(number, decimals, MidpointRounding.AwayFromZero);
         }
